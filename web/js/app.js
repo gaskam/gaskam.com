a1caa91771c53c6b86e2fe10b9d927edb0a1e043
Patafix <project.patafix@gmail.com>
2022-07-06 12:01:12 +0200

    background image

---
+++ web/js/app.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function ($) {
+
+    $(function () {
+        $(window).on("scroll", function () {
+            if ($(window).scrollTop() > 50) {
+                $("header").addClass("scroll");
+            } else {
+                $("header").removeClass("scroll");
+            }
+        });
+    })
+
+});
