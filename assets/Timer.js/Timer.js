805a326286a12df1075b4f2043b783f514091c8d
Bot-maker <leys.kamil@gmail.com>
2022-07-15 19:16:08 +0200

    big commit

--- assets/Timer.js/Timer.js
+++ assets/Timer.js/Timer.js
@@ -9,69 +9,106 @@ export class Timer {
     beginTime = null;
     paused = true;
     callbacks = [];
-    constructor(duration, options) {
-        this.duration = duration;
-        this.options = options || {};
-    }
     formatTime(time, format) {
         format = format || "ms";
         format = format.toLowerCase();
         if (format === "ms") return time;
         if (format.includes("yyyy")) {
-            let years = Math.floor(time / 31536000000);
-            format = format.replace("yyyy", years);
-            time -= years * 31536000000; // 31 536 000 000 ms = 1 year of 365 days
+            let tmp = Math.floor(time / 31536000000);
+            format = format.replace("yyyy", tmp);
+            time -= tmp * 31536000000; // 31 536 000 000 ms = 1 year of 365 days
         }
         else if (format.includes("yy")) {
-            let years = Math.floor(time / 31536000000);
-            format = format.replace("yy", String(years).slice(-2));
-            time -= years * 31536000000; // 31 536 000 000 ms = 1 year of 365 days
+            let tmp = Math.floor(time / 31536000000);
+            format = format.replace("yy", String(tmp).slice(-2));
+            time -= tmp * 31536000000; // 31 536 000 000 ms = 1 year of 365 days
         }
         if (format.includes("mo")) {
-            let months = Math.floor(time / 2592000000);
-            format = format.replace("mo", months);
-            time -= months * 2592000000; // 2 592 000 000 ms = 1 month of 30 days
+            let tmp = Math.floor(time / 2592000000);
+            format = format.replace("mo", tmp);
+            time -= tmp * 2592000000; // 2 592 000 000 ms = 1 month of 30 days
         }
         if (format.includes("ww")) {
-            let weeks = Math.floor(time / 604800000);
-            format = format.replace("ww", weeks);
-            time -= weeks * 604800000; // 604 800 000 ms = 1 week
+            let tmp = Math.floor(time / 604800000);
+            format = format.replace("ww", tmp);
+            time -= tmp * 604800000; // 604 800 000 ms = 1 week
         }
         if (format.includes("dd")) {
-            let days = Math.floor(time / 86400000);
-            format = format.replace("dd", days);
-            time -= days * 86400000; // 86 400 000 ms = 1 day
+            let tmp = Math.floor(time / 86400000);
+            format = format.replace("dd", tmp);
+            time -= tmp * 86400000; // 86 400 000 ms = 1 day
         }
         if (format.includes("hh")) {
-            let hours = Math.floor(time / 3600000);
-            format = format.replace("hh", hours);
-            time -= hours * 3600000; // 3 600 000 ms = 1 hour
+            let tmp = Math.floor(time / 3600000);
+            format = format.replace("hh", tmp);
+            time -= tmp * 3600000; // 3 600 000 ms = 1 hour
         }
         if (format.includes("mm")) {
-            let minutes = Math.floor(time / 60000);
-            format = format.replace("mm", minutes);
-            time -= minutes * 60000; // 60 000 ms = 1 minute
+            let tmp = Math.floor(time / 60000);
+            format = format.replace("mm", tmp);
+            time -= tmp * 60000; // 60 000 ms = 1 minute
         }
         if (format.includes("ss")) {
-            let seconds = Math.floor(time / 1000);
-            format = format.replace("ss", seconds);
-            time -= seconds * 1000; // 1 000 ms = 1 second
+            let tmp = Math.floor(time / 1000);
+            format = format.replace("ss", tmp);
+            time -= tmp * 1000; // 1 000 ms = 1 second
         }
         if (format.includes("ms")) {
-            let milliseconds = time;
-            format = format.replace("ms", milliseconds);
+            format = format.replace("ms", time);
         }
         return format;
     }
+    toMs(time) {
+        try {
+            let tmp = "";
+            if (typeof time === "string") {
+                if (time.includes("ms")) return Number(time.replace("ms", ""));
+                if (time.includes("ss")) return Number(time.replace("ss", "")) * 1000;
+                if (time.includes("s")) return Number(time.replace("s", "")) * 1000;
+                if (time.includes("mm")) return Number(time.replace("mm", "")) * 60000;
+                if (time.includes("m")) return Number(time.replace("m", "")) * 60000;
+                if (time.includes("hh")) return Number(time.replace("hh", "")) * 3600000;
+                if (time.includes("h")) return Number(time.replace("h", "")) * 3600000;
+                if (time.includes("dd")) return Number(time.replace("dd", "")) * 86400000;
+                if (time.includes("d")) return Number(time.replace("d", "")) * 86400000;
+                if (time.includes("ww")) return Number(time.replace("ww", "")) * 604800000;
+                if (time.includes("w")) return Number(time.replace("w", "")) * 604800000;
+                if (time.includes("mo")) return Number(time.replace("mo", "")) * 2592000000;
+                if (time.includes("yyyy")) return Number(time.replace("yyyy", "")) * 31536000000;
+                if (time.includes("yy")) return Number(time.replace("yy", "")) * 31536000000;
+                if (time.includes("y")) return Number(time.replace("y", "")) * 31536000000;
+            }
+            if (typeof time === "number") {
+                return time;
+            }
+        } catch (e) {
+            console.error("Timer.js: incorrect value provided for time");
+        }
+        return 0;
+    }
+    constructor(duration, options) {
+        this.duration = this.toMs(duration);
+        this.options = options || {};
+    }
+    
+    // Timer length functions
+    setDuration(duration) {
+        this.duration = this.toMs(duration);
+    }
+    addRunningTime(time) {
+        this.pauseAmount += this.toMs(time);
+    }
+    addDuration(duration) {
+        this.duration += this.toMs(duration);
+    }
+    
+    // Timer execution functions
     runningTime(format) {
         if (this.paused === true) return this.formatTime(this.pauseAmount, format);
         return this.formatTime(this.pauseAmount + (Date.now() - this.beginTime), format);
     }
     timeLeft(format) {
         return this.formatTime(this.duration - this.runningTime(), format);
-    }
-    isRunning() {
-        return this.paused !== true;
     }
     start() {
         this.paused = false;
@@ -100,29 +137,51 @@ export class Timer {
     }
 
     // Callbacks functions
-    addEventListener(event, callback){
-        this.callbacks.push(Object.defineProperty({}, event, {
+    addEventListener(event, callback) {
+        return this.callbacks.push(Object.defineProperty({}, event, {
             value: callback,
             writable: true
         }));
-        return this;
     }
-    on(event, callback){
+    on(event, callback) {
         return this.addEventListener(event, callback);
     }
 
-    dispatchEvent(event){
-        this.callbacks.forEach((el)=>{
-            console.log(typeof el)
-            if(el.hasOwnProperty(event)){
-                el[event]();
-            }
-        });
-        return this;
+    removeEventListener(event) {
+        if (typeof event === "string") {
+            this.callbacks = this.callbacks.filter(callback => callback[event] == undefined);
+            return this;
+        }
+        if (typeof event === "number") {
+            this.callbacks.splice(event, 1);
+            return this;
+        }
     }
 
-    emit(event){
+    dispatchEvent(event) {
+        let tmp = 0;
+        this.callbacks.forEach((el) => {
+            if (el.hasOwnProperty(event)) {
+                el[event]();
+                tmp++;
+            }
+        });
+        return tmp;
+    }
+
+    emit(event) {
         return this.dispatchEvent(event);
+    }
+
+
+    // Events
+    checkEvents() {
+        if (this.paused === true) return;
+        if (this.timeLeft() <= 0) {
+            this.emit("end");
+            this.stop();
+        }
+
     }
 
 }
