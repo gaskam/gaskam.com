164896c12f94872e44f91d70a7f01ce02b5f790f
Bot-maker <leys.kamil@gmail.com>
2022-07-17 20:49:38 +0200

    big Commit(with minified version too) <@572810487390011403>

--- assets/Timer.js/Timer.js
+++ assets/Timer.js/Timer.js
@@ -2,13 +2,23 @@
 // Made with ❤️ by Gaskam -> Gaskam.dev
 // Version: 0.0.1 Pre-Alpha
 // Released: Event gestionnary
-// TODO: Timing events triggering
+// TODO: Timing events triggering (on the fly)
 
 export class Timer {
     pauseAmount = 0;
     beginTime = null;
     paused = true;
     callbacks = [];
+    _timeline = [];
+    specialEventsRegistry = [];
+    _currentTimeoutId = -1;
+    options = {
+        eventPrecision: 25
+    }
+
+    now() {
+        return Date.now();
+    }
     formatTime(time, format) {
         format = format || "ms";
         format = format.toLowerCase();
@@ -25,6 +35,7 @@ export class Timer {
         }
         if (format.includes("mo")) {
             let tmp = Math.floor(time / 2592000000);
+            if (tmp < 10) tmp = "0" + tmp;
             format = format.replace("mo", tmp);
             time -= tmp * 2592000000; // 2 592 000 000 ms = 1 month of 30 days
         }
@@ -35,47 +46,54 @@ export class Timer {
         }
         if (format.includes("dd")) {
             let tmp = Math.floor(time / 86400000);
+            if (tmp < 10) tmp = "0" + tmp;
             format = format.replace("dd", tmp);
             time -= tmp * 86400000; // 86 400 000 ms = 1 day
         }
         if (format.includes("hh")) {
             let tmp = Math.floor(time / 3600000);
+            if (tmp < 10) tmp = "0" + tmp;
             format = format.replace("hh", tmp);
             time -= tmp * 3600000; // 3 600 000 ms = 1 hour
         }
         if (format.includes("mm")) {
             let tmp = Math.floor(time / 60000);
+            if (tmp < 10) tmp = "0" + tmp;
             format = format.replace("mm", tmp);
             time -= tmp * 60000; // 60 000 ms = 1 minute
         }
         if (format.includes("ss")) {
             let tmp = Math.floor(time / 1000);
+            if (tmp < 10) tmp = "0" + tmp;
             format = format.replace("ss", tmp);
             time -= tmp * 1000; // 1 000 ms = 1 second
         }
         if (format.includes("ms")) {
-            format = format.replace("ms", time);
+            let tmp = Math.floor(time);
+            if (tmp < 100) tmp = "00" + tmp;
+            else if (tmp < 10) tmp = "0" + tmp;
+            format = format.replace("ms", tmp);
         }
         return format;
     }
     toMs(time) {
         try {
-            let tmp = "";
             if (typeof time === "string") {
+                time = time.toLowerCase();
                 if (time.includes("ms")) return Number(time.replace("ms", ""));
                 if (time.includes("ss")) return Number(time.replace("ss", "")) * 1000;
-                if (time.includes("s")) return Number(time.replace("s", "")) * 1000;
                 if (time.includes("mm")) return Number(time.replace("mm", "")) * 60000;
-                if (time.includes("m")) return Number(time.replace("m", "")) * 60000;
                 if (time.includes("hh")) return Number(time.replace("hh", "")) * 3600000;
-                if (time.includes("h")) return Number(time.replace("h", "")) * 3600000;
                 if (time.includes("dd")) return Number(time.replace("dd", "")) * 86400000;
-                if (time.includes("d")) return Number(time.replace("d", "")) * 86400000;
                 if (time.includes("ww")) return Number(time.replace("ww", "")) * 604800000;
-                if (time.includes("w")) return Number(time.replace("w", "")) * 604800000;
                 if (time.includes("mo")) return Number(time.replace("mo", "")) * 2592000000;
                 if (time.includes("yyyy")) return Number(time.replace("yyyy", "")) * 31536000000;
                 if (time.includes("yy")) return Number(time.replace("yy", "")) * 31536000000;
+                if (time.includes("s")) return Number(time.replace("s", "")) * 1000;
+                if (time.includes("m")) return Number(time.replace("m", "")) * 60000;
+                if (time.includes("h")) return Number(time.replace("h", "")) * 3600000;
+                if (time.includes("d")) return Number(time.replace("d", "")) * 86400000;
+                if (time.includes("w")) return Number(time.replace("w", "")) * 604800000;
                 if (time.includes("y")) return Number(time.replace("y", "")) * 31536000000;
             }
             if (typeof time === "number") {
@@ -88,31 +106,37 @@ export class Timer {
     }
     constructor(duration, options) {
         this.duration = this.toMs(duration);
-        this.options = options || {};
+        this.options = options || this.options;
     }
-    
+
     // Timer length functions
     setDuration(duration) {
         this.duration = this.toMs(duration);
     }
-    addRunningTime(time) {
-        this.pauseAmount += this.toMs(time);
-    }
     addDuration(duration) {
         this.duration += this.toMs(duration);
     }
-    
+    setRunningTime(time) {
+        this.pauseAmount = this.toMs(time);
+        this.beginTime = this.now();
+    }
+    addRunningTime(time) {
+        this.pauseAmount += this.toMs(time);
+    }
+
     // Timer execution functions
     runningTime(format) {
         if (this.paused === true) return this.formatTime(this.pauseAmount, format);
-        return this.formatTime(this.pauseAmount + (Date.now() - this.beginTime), format);
+        return this.formatTime(this.pauseAmount + (this.now() - this.beginTime), format);
     }
     timeLeft(format) {
         return this.formatTime(this.duration - this.runningTime(), format);
     }
     start() {
-        this.paused = false;
-        this.beginTime = Date.now();
+        if (this.paused) {
+            this.paused = false;
+            this.beginTime = this.now();
+        }
         return this;
     }
     resume() {
@@ -121,7 +145,7 @@ export class Timer {
     stop() {
         if (this.paused !== true) {
             this.paused = true;
-            this.pauseAmount += Date.now() - this.beginTime;
+            this.pauseAmount += this.now() - this.beginTime;
             return this;
         }
         return this;
@@ -138,10 +162,8 @@ export class Timer {
 
     // Callbacks functions
     addEventListener(event, callback) {
-        return this.callbacks.push(Object.defineProperty({}, event, {
-            value: callback,
-            writable: true
-        }));
+        this._registerEvent(event);
+        return this.callbacks.push([event, callback]);
     }
     on(event, callback) {
         return this.addEventListener(event, callback);
@@ -149,7 +171,7 @@ export class Timer {
 
     removeEventListener(event) {
         if (typeof event === "string") {
-            this.callbacks = this.callbacks.filter(callback => callback[event] == undefined);
+            this.callbacks = this.callbacks.filter(tmp => tmp[0] !== "event");
             return this;
         }
         if (typeof event === "number") {
@@ -161,8 +183,8 @@ export class Timer {
     dispatchEvent(event) {
         let tmp = 0;
         this.callbacks.forEach((el) => {
-            if (el.hasOwnProperty(event)) {
-                el[event]();
+            if (el[0] == event) {
+                el[1]();
                 tmp++;
             }
         });
@@ -173,15 +195,64 @@ export class Timer {
         return this.dispatchEvent(event);
     }
 
-
+    // Events registering functions
+    _timelineInsert(date, name) {
+        let i = 0;
+        try {
+            while (date >= this._timeline[i][0]) {
+                if (name === this._timeline[i][1]) return this;
+                i++;
+            }
+        } catch (e) {
+            i = this._timeline.length;
+        }
+        this._timeline.splice(i, 0, [date, name]);
+    }
+    _registerEvent(event) {
+        if (typeof event !== Array) event = [event];
+        event[0].toLowerCase();
+        let tmp = this.toMs(event[0]);
+        if (tmp > 0) {
+            let nextTime = this.now() + tmp;
+            this._timelineInsert(nextTime, event[0]);
+        } else {
+            specialEventsRegistry.push(event[0]);
+        }
+        return this;
+    }
+    _unregisterEvent(event) {
+        if (typeof event === Array) event = event[0];
+        if (typeof event === "string") event = event.toLowerCase();
+        let tmp = this.toMs(event);
+        if (tmp > 0) {
+            this._timeline = this._timeline.filter(tmp => tmp[1] !== event);
+        } else {
+            specialEventsRegistry = specialEventsRegistry.filter(tmp => tmp !== event);
+        }
+        return this;
+    }
+    _registerAllEvents() {
+        this._timeline = [];
+        this.callbacks.forEach((tmp) => {
+            this._registerEvent(tmp);
+        });
+    }
     // Events
     checkEvents() {
-        if (this.paused === true) return;
-        if (this.timeLeft() <= 0) {
-            this.emit("end");
-            this.stop();
+        console.log("checkEvents");
+        while(this._timeline.length && this._timeline[0][0]+this.options.eventPrecision <= this.now()) {
+            console.log("event triggered: " + this._timeline[0][1]);
+            this.dispatchEvent(this._timeline[0][1]);
+            let tmp = this._timeline.shift();
+            this._registerEvent(tmp[1]);
         }
-
+        if (this.timeLeft() <= 0 && this.paused == false) {
+            this.dispatchEvent("end");
+        }
+        if (this._currentTimeoutId !== null && this._timeline.length) {
+            clearTimeout(this._currentTimeoutId);
+            if (this._timeline.length) this._currentTimeoutId = setTimeout(this.checkEvents.bind(this), this._timeline[0][0] - this.now());
+        }
     }
 
 }
