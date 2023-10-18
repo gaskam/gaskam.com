1d9110f646c35b2068d9978061eb6d36a54978cc
Patafix <gaspard.lebaube@gaskam.com>
2023-10-18 14:51:45 +0200

    Better button accessibility

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -3,30 +3,31 @@ import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
 
 export default function Error404() {
-return (
-<>
+    return (
+        <>
+            <Head>
+                <title>404 - Page not found</title>
+                <link rel="stylesheet" href="/css/_404.css" />
+            </Head>
+            <Header />
 
-    <Head>
-        <title>404 - Page not found</title>
-        <link rel="stylesheet" href="/css/_404.css" />
-    </Head>
-    <Header />
-
-    <error404>
-
-        <body>
-            <div class="content">
-                <img src="/images/gaskam-logo-min.png"
-                    alt="the Gaskam logo: a tagged background with the text 'Gaskam' in the middle" />
-                <h1>404 - Page not found</h1>
-                <p>
-                    The page you were looking for doesn't exist.
-                </p>
-                <a href="https://gaskam.com/">Go back home</a>
-            </div>
-        </body>
-
-    </error404>
-</>
-);
+            <error404>
+                <body>
+                    <div class="content">
+                        <img
+                            src="/images/gaskam-logo-min.png"
+                            alt="the Gaskam logo: a tagged background with the text 'Gaskam' in the middle"
+                        />
+                        <h1>404 - Page not found</h1>
+                        <p>
+                            The page you were looking for doesn't exist.
+                        </p>
+                        <div className="button">
+                            <a href="https://gaskam.com/">Go back home</a>
+                        </div>
+                    </div>
+                </body>
+            </error404>
+        </>
+    );
 }
