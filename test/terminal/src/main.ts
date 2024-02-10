e8ddfeec2532634e7639bcf08d3c2c0b4a103bd0
Patafix <87929402+PatafixPLTX@users.noreply.github.com>
2024-02-10 00:20:16 +0000

    Try to implement env

--- test/terminal/src/main.ts
+++ test/terminal/src/main.ts
@@ -1,12 +1,15 @@
 import './style.css'
 import './app.ts'
+import './commands.ts'
 import { defaultMessage } from './commands.ts'
+
+// console.log(process.env.SECRET) // Error: process is not defined but i don't know why
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="div">
     <div id="terminal" class="terminal">
     ${defaultMessage}
     </div>
-    <span>gaskam.com $&nbsp;</span><span id="input" class="input selectable"></span><span id="cursor" class="cursor"></span>
+    <br><span class="session">gaskam.com $&nbsp;</span><span id="input" class="input selectable"></span><span id="cursor" class="cursor"></span>
   </div>
 `
