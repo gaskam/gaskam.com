6c2fa365ff161f108efc54c98e8fd5c9259008d4
Patafix <gaspard.lebaube@gaskam.com>
2024-02-13 22:47:41 +0100

    Minified code

--- test/terminal/src/main.ts
+++ test/terminal/src/main.ts
@@ -1,13 +1,1 @@
-import "./style.css";
-import "./app.ts";
-import "./commands.ts";
-import { defaultMessage } from "./commands.ts";
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="div">
-    <div id="terminal" class="terminal">
-    ${defaultMessage}
-    </div>
-    <br><span id="session" class="session">gaskam.com $&nbsp;</span><span id="input" class="input selectable"></span><span id="cursor" class="cursor"></span>
-  </div>
-`;
+import"./style.css";import"./app.ts";import"./commands.ts";import{defaultMessage}from"./commands.ts";document.querySelector<HTMLDivElement>("#app")!.innerHTML=`<div class="div"><div id="terminal" class="terminal">${defaultMessage}</div><br><span id="session" class="session">gaskam.com $&nbsp;</span><span id="input" class="input selectable"></span><span id="cursor" class="cursor"></span></div>`;
