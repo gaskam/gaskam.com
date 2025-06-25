bfb2d5f0f578479b77bd148988e6a532e50c3e1d
kamil-leys <72702161+kamil-leys@users.noreply.github.com>
2025-06-26 00:41:26 +0200

    Update

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 Bun.serve({
 	development: true,
 	fetch(req) {
-		console.log(req.url);
-		return new Response(file(req.
+		const url = new URL(req.url);
+		return new Response(Bun.file(url.pathname.slice(1) || "index.html"));
 	},
 });
